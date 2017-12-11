993227c39b04ef8cfba0f4c74b27f3247296ac83
Rodrigo Barreto <rodrigo.barreto@mall-connect.com>
2017-12-12 00:32:09 +0100

    test_app/spec/httparty

---
+++ test_app/spec/httparty/httparty_spec.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+describe 'HTTParty' do
+    #it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
+    it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes } do
+      #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+      #     to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+  
+      response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/3")
+      content_type = response.headers['content-type']
+      expect(content_type).to match(/application\/json/)
+    end
+  end
